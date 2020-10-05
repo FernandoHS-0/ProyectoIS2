aca2f0c8fce86e0f329b1eb40434b855cbf0125a
Fernando Hernández <59585701+FernandoHS-0@users.noreply.github.com>
2020-10-04 21:37:41 -0500

    Overbook preparado
    Se modificó la forma de reservar, se muestran los lugares ocupados y si no hay ninguno pasa a ser overbook

--- cliente.h
+++ cliente.h
@@ -44,6 +44,12 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
 private:
     Ui::Cliente *ui;
     QSqlDatabase conexion;
